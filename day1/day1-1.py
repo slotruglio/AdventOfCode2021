fa2085eecddc6444dc68b6e8576caccfb7f64b0f
slotruglio <slotruglio@gmail.com>
2021-12-04 23:20:35 +0100

    day1-1 optimization

--- day1/day1-1.py
+++ day1/day1-1.py
@@ -1,21 +1,17 @@
-import os
-import sys
+with open("input.txt", "r") as f:
+    lines = f.readlines()
 
-f = open("input.txt", "r")
-lines = f.readlines()
+    isFirst = True
+    cValue = 0
+    counter = 0
 
-isFirst = True
-cValue = 0
-counter = 0
+    for line in lines:
+        line = line.strip()
+        if isFirst:
+            isFirst = False
+        else:
+            if cValue < int(line) :
+                counter +=1
+        cValue = int(line)
 
-for line in lines:
-    line = line.strip()
-    if isFirst:
-        isFirst = False
-    else:
-        if cValue < int(line) :
-            counter +=1
-    cValue = int(line)
-
-print("Total increment = ", counter)    
-f.close()
+    print("Total increment = ", counter)
