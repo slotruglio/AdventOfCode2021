0c01695f7175ad601886e0b1964b708ed6f174ae
slotruglio <slotruglio@gmail.com>
2021-12-07 18:33:10 +0100

    fixed bench

--- benchmarker.py
+++ benchmarker.py
@@ -1,13 +1,13 @@
 import time
 import os
 from datetime import datetime
-from subprocess import call
+import subprocess 
 
 start_path = '.' # current directory
 options = []
 for path,dirs,files in os.walk(start_path):
     for filename in files:
-        if filename.count("py") > 0  and filename.count("day") > 0:
+        if filename.count("py") > 0  and filename.count("day") > 0 and path.count("bench") == 0:
             options.append(os.path.join(path,filename))
 
 options.sort()
@@ -22,19 +22,24 @@ choice = int(input("What py do you want to execute? "))
 program = options[choice].split('/')[2]
 folder = options[choice].split('/')[1]
 
+timex = datetime.now()
 print("Execution of:", program)
+text = ""
 times = []
 for i in range(0, 10):
     start = time.time()
 
     os.chdir(folder)
-    call(["python", program])
+    tmp = subprocess.check_output(["python", program]).decode("utf-8")
+    text += tmp
     os.chdir("..")
     stop = time.time() - start
     times.append(stop)
+    print(tmp)
     print("Time of execution[{}]: {}".format(i, stop))
 
-f = open("bench/{}{}.txt".format(program, datetime.now()), "w")
+f = open("bench/{}{}.txt".format(program, timex), "a")
+f.write(text)
 f.write("Average time of execution:{}".format(sum(times)/len(times)))
 f.close()
 print("Average time of execution: ", sum(times)/len(times))
