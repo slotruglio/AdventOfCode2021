37e37409dab2d3f7fb6198a2dac6f7cdfcdf3ecf
slotruglio <slotruglio@gmail.com>
2021-12-07 18:11:43 +0100

    new script to run benchmarks

---
+++ benchmarker.py
@@ -0,0 +1,37 @@
+import time
+import os
+from subprocess import call
+
+start_path = '.' # current directory
+options = []
+for path,dirs,files in os.walk(start_path):
+    for filename in files:
+        if filename.count("py") > 0  and filename.count("day") > 0:
+            options.append(os.path.join(path,filename))
+
+options.sort()
+for i in range(0, len(options)):
+    toPrint = ""
+    if i < 10:
+        toPrint = "{})  {}".format(i, options[i])
+    else: toPrint = "{}) {}".format(i, options[i])
+    print(toPrint)
+
+choice = int(input("What py do you want to execute? "))
+program = options[choice].split('/')[2]
+folder = options[choice].split('/')[1]
+
+print("Execution of:", program)
+times = []
+for i in range(0, 10):
+    start = time.time()
+
+    os.chdir(folder)
+    call(["python", program])
+    os.chdir("..")
+    stop = time.time() - start
+    times.append(stop)
+    print("Time of execution[{}]: {}".format(i, stop))
+
+
+print("Average time of execution:", sum(times)/len(times))
