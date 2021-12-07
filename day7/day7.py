5906c831d992a9443b16e935a102daeded5bdeec
slotruglio <slotruglio@gmail.com>
2021-12-07 10:31:55 +0100

    optimization for part 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,21 +1,27 @@
 import time
 
-start = time.time()
-positions = []
-with open("input.txt", "r") as f:
-    positions = [int(e) for e in f.readline().split(',')]
+def day7():
+    positions = []
+    with open("input.txt", "r") as f:
+        positions = [int(e) for e in f.readline().split(',')]
 
-distance = [0 for e in positions]
-distance2 = distance.copy()
-for i in range(0, len(positions)):
+    distance = [0 for e in positions]
+    distance2 = distance.copy()
+    for i in range(0, len(positions)):
+        for x in range(0, len(positions)):
+            counter = abs(positions[x]-positions[i])
+            distance[i] += counter
+            distance2[i] += int(counter*(counter+1)/2)
 
-    for e in positions:
-        counter = abs(e-positions[i])
-        distance[i] += counter
-        for j in range(1, counter+1):
-            distance2[i] += j
+    print("#Day7 - Part1:", min(distance))
+    print('#Day7 - Part2:', min(distance2))   
 
-print("#Day7 - Part1:", min(distance))
-print('#Day7 - Part2:', min(distance2))
+times = []
+for i in range(0, 10):
+    start = time.time()
+    day7()
+    stop = time.time() - start
+    times.append(stop)
+    print("Time of execution[{}]: {}".format(i, stop))
 
-print("Time of execution: ", time.time()-start)
+print("Average time of execution:", sum(times)/len(times))
